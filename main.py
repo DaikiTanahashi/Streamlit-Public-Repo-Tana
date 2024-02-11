9e0fa6337d7ee4722a95515f929ef9be0610be28
Daiki Tanahashi <128291912+DaikiTanahashi@users.noreply.github.com>
2024-02-11 13:06:36 +0900

    fistr somitt

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
-import streamlit as st
 import pandas as pd
-from io import StringIO
+import streamlit as st
+from chardet import detect
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+from app.test import show_options_with_filter
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+def get_df_from_uploaded_file(uploaded_file) -> pd.DataFrame:
+    st.write(uploaded_file.name)
+    file_name = uploaded_file.name
+    file_extension = file_name.split(".")[-1]  # 拡張子を取得
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    st.write(file_extension)
+
+    if "xls" in file_extension:
+        df = pd.read_excel(uploaded_file)
+        return df
+    elif file_extension == "csv":
+        binary_data = uploaded_file.getvalue()
+        encode_data = detect(binary_data)
+        st.write(encode_data)
+        encoding = encode_data["encoding"]
+        df = pd.read_csv(uploaded_file, encoding=encoding)
+        return df
+
+
+def main():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        uploaded_file = st.file_uploader("Choose a left_file")
+        if uploaded_file is not None:
+            df1 = get_df_from_uploaded_file(uploaded_file)
+            st.write(df1)
+            options = df1.columns.tolist()
+            df1_key = show_options_with_filter(options, "df1")
+            st.write(df1_key)
+
+    with col2:
+        uploaded_file = st.file_uploader("Choose a right_file")
+        if uploaded_file is not None:
+            df2 = get_df_from_uploaded_file(uploaded_file)
+            st.write(df2)
+            options = df2.columns.tolist()
+            df2_key = show_options_with_filter(options, "df2")
+            st.write(df2_key)
+
+
+if __name__ == "__main__":
+    main()
