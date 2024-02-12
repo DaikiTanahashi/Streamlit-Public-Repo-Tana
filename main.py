b4a6ec4c90ca58d12e20362052abb91137680951
Daiki Tanahashi <128291912+DaikiTanahashi@users.noreply.github.com>
2024-02-12 13:21:58 +0900

    commit

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import streamlit as st
 
 from app.df_component import df_component
 
+
 def main():
     col1, col2 = st.columns(2)
 
     with col1:
-        df1=df_component("df1")
+        df1 = df_component("df1")
         df1.show_df()
         df1.show_options_with_filter()
 
     with col2:
-        df2=df_component("df2")
+        df2 = df_component("df2")
         df2.show_df()
         df2.show_options_with_filter()
 
