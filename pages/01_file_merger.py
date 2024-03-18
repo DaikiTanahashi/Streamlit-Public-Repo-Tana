6665dd8e5d449116a68e32f7122183e668647a70
Daiki Tanahashi <128291912+DaikiTanahashi@users.noreply.github.com>
2024-03-18 20:17:08 +0900

    完成～～

--- pages/01_file_merger.py
+++ pages/01_file_merger.py
@@ -1,3 +1,5 @@
+from main import setting
 from page_01.main_01 import file_merger
 
+setting()
 file_merger()
--- pages/02_Import_to_template.py
+++ pages/02_Import_to_template.py
@@ -1,3 +1,5 @@
+from main import setting
 from page_02.main_02 import import_to_template
 
+setting()
 import_to_template()
